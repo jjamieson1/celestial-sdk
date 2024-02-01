a0b1172fe86f609c6b4054ca2cb9d466197a4bb2
jjamieson1 <jjamie@gmail.com>
2024-02-01 11:23:35 -0700

    removed returning the hashed password

--- models/UserModel.go
+++ models/UserModel.go
@@ -8,7 +8,7 @@ type User struct {
 	Emails             []Email   `json:"emails,omitempty"`
 	Phones             []Phone   `json:"phones,omitempty"`
 	Addresses          []Address `json:"addresses,omitempty"`
-	Password           []byte    `json:"password,omitempty"`
+	Password           []byte    `json:"-"`
 	HashedPassword     []byte    `json:"-"`
 	Roles              []Role    `json:"roles,omitempty"`
 	Active             bool      `json:"active"`
